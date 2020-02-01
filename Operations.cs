b5814c794c9b9b9cbc90dde6b45711dfe81a15d5
Lacowmi <55349470+Lacowmi@users.noreply.github.com>
2020-02-01 19:10:36 +0200

    Add files via upload

---
+++ Operations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Operations
+    {
+        public int division(int a,int b)
+        {
+            return a / b;
+        }
+        public int multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        public int addition(int a, int b)
+        {
+            return a + b;
+        }
+        public int substraction(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations operations = new Operations();
+            int a, b, result = 0;
+            String operation;
+
+            Console.Write("Введите первое число: ");
+            while(!int.TryParse(Console.ReadLine(),out a))
+            {
+                Console.Write("Введите корректное значение!\nВведите первое число: ");
+            }
+            Console.Write("Введите второе число: ");
+            while (!int.TryParse(Console.ReadLine(), out b))
+            {
+                Console.Write("Введите корректное значение!\nВведите второе число: ");
+            }
+
+            Console.Write("Выберете операцию(/, *, +, -): ");
+            operation = Console.ReadLine();
+            while(operation != "/" && operation != "*" && operation != "+" && operation != "-")
+            {
+                Console.Write("Введите корректную операцию: ");
+                operation = Console.ReadLine();
+            }
+            switch (operation)
+            {
+                case "/":
+                    result = operations.division(a, b);
+                    break;
+                case "*":
+                    result = operations.multiplication(a, b);
+                    break;
+                case "+":
+                    result = operations.addition(a, b);
+                    break;
+                case "-":
+                    result = operations.substraction(a, b);
+                    break;
+            }
+            Console.WriteLine("Результат: " + result);
+            Console.ReadKey();
+        }
+    }
+}
